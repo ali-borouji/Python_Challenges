50ef3c4f73ddcf6ffeb16a663c43f6dd4fdd552d
Ali Borouji <aliboroujimusic@gmail.com>
2025-04-19 17:50:37 +0330

    payment.py
    1st version

--- payment.py
+++ payment.py
@@ -1,11 +1,14 @@
 print('Hello Dear')
 print('Hopefully this payment method seems fair enough!')
+print('Please use ONLY numbers for the following questions.')
 h = float(input('Please Enter The Hours Wasted This Month!:'))
 r = float(input('How much do you charge your boss per hour? :D'))
 if h <= 160:
   print('You really care about the Life-Work balance, huh?')
   pay = (h * r)
-  print('That is what you get $', pay)
+  print('This is what you get $', pay)
+  yn = input('Is it worth it?!')
+  print('Oh I forgot to mention. This is your gross payment! Good luck.')
 else:
   h > 160
   print('you work so hard!')
@@ -14,3 +17,4 @@ else:
   print('$', pay)
   yn = input('Is it worth it?!')
   print('Oh I forgot to mention. This is your gross payment! Good luck.')
+print('And please do NOT take it personal, you know I love you <3')
